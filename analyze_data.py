46654a072d9eb7000f277d6b05144dcef5299949
Lucas <lucaswarwick02@gmail.com>
2022-11-28 14:24:47 +0000

    Seperate HD into their own plots

--- analyze_data.py
+++ analyze_data.py
@@ -11,45 +11,47 @@ def display_base_and_vac (agg_df: pd.DataFrame, agg_vac_df: pd.DataFrame):
     
     y_max = agg_df.iloc[0]['Susceptible'] + agg_df.iloc[0]['Infected']
     
-    fig, axes = plt.subplots(1, 3, figsize=(14, 4))
+    fig, axes = plt.subplots(2, 2, figsize=(12, 10))
+    plt.ylim(0, y_max)
     
     legend_loc = 'upper right'
     
     # * No Vaccination
     
-    add_plot(axes[0], agg_df, 'Susceptible', 'blue')
-    add_plot(axes[0], agg_df, 'Infected', 'orange')
-    add_plot(axes[0], agg_df, 'Recovered', 'green')
-    add_plot(axes[0], agg_df, 'Hospitalised', 'yellow')
-    add_plot(axes[0], agg_df, 'Dead', 'red')
+    add_plot(axes[0][0], agg_df, 'Susceptible', 'blue')
+    add_plot(axes[0][0], agg_df, 'Infected', 'red')
+    add_plot(axes[0][0], agg_df, 'Recovered', 'green')
 
-    axes[0].legend(loc=legend_loc)
-    axes[0].set_title('Number of Individuals vs Time')
-    axes[0].set(xlabel='Time', ylabel='Number of Individuals')
+    axes[0][0].legend(loc=legend_loc)
+    axes[0][0].set_title('Number of Individuals vs Time')
+    axes[0][0].set(xlabel='Time', ylabel='Number of Individuals')
+    
+    add_plot(axes[1][0], agg_df, 'Hospitalised', 'orange')
+    add_plot(axes[1][0], agg_df, 'Dead', 'red')
+    
+    axes[1][0].legend(loc=legend_loc)
+    axes[1][0].set_title('Number of Individuals vs Time')
+    axes[1][0].set(xlabel='Time', ylabel='Number of Individuals')
     
     # * Global Vaccination
     
-    add_plot(axes[1], agg_vac_df, 'Susceptible', 'blue')
-    add_plot(axes[1], agg_vac_df, 'Infected', 'orange')
-    add_plot(axes[1], agg_vac_df, 'Recovered', 'green')
-    add_plot(axes[1], agg_vac_df, 'Vaccinated', 'purple')
-    add_plot(axes[1], agg_vac_df, 'Hospitalised', 'yellow')
-    add_plot(axes[1], agg_vac_df, 'Dead', 'red')
+    add_plot(axes[0][1], agg_vac_df, 'Susceptible', 'blue')
+    add_plot(axes[0][1], agg_vac_df, 'Infected', 'red')
+    add_plot(axes[0][1], agg_vac_df, 'Recovered', 'green')
+    add_plot(axes[0][1], agg_vac_df, 'Vaccinated', 'purple')
     
-    axes[1].legend(loc=legend_loc)
-    axes[1].set_title('Number of Individuals vs Time')
-    axes[1].set(xlabel='Time', ylabel='Number of Individuals')
+    axes[0][1].legend(loc=legend_loc)
+    axes[0][1].set_title('Number of Individuals vs Time')
+    axes[0][1].set(xlabel='Time', ylabel='Number of Individuals')
     
-    # * Cumulative Infected vs Time
+    add_plot(axes[1][1], agg_vac_df, 'Hospitalised', 'orange')
+    add_plot(axes[1][1], agg_vac_df, 'Dead', 'red')
     
-    # axes[2].plot(agg_df['Time'], agg_df['CumulativeInfected'], linestyle='dashed', label='No Vaccination', color='black')
-    # axes[2].plot(agg_vac_df['Time'], agg_vac_df['CumulativeInfected'], label='Global Vacciantion', color='black')
+    axes[1][1].legend(loc=legend_loc)
+    axes[1][1].set_title('Number of Individuals vs Time')
+    axes[1][1].set(xlabel='Time', ylabel='Number of Individuals')
     
-    # axes[2].legend(loc=legend_loc)
-    # axes[2].set_title('Cumulative Infected vs Time')
-    # axes[2].set(xlabel='Time', ylabel='Cumulative Infected')
-    plt.ylim(0, y_max)
-    
+    plt.setp(axes, ylim=(0, 2500))
     plt.show()
     
 
