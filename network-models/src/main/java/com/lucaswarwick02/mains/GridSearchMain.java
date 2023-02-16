f93740676208e36933d9d574a6028cf8bfa3c4fa
Lucas <lucaswarwick02@gmail.com>
2023-02-16 18:07:50 +0000

    Results for oneoff 50%

--- network-models/src/main/java/com/lucaswarwick02/mains/GridSearchMain.java
+++ network-models/src/main/java/com/lucaswarwick02/mains/GridSearchMain.java
@@ -39,7 +39,7 @@ public class GridSearchMain {
         }
 
         // * OneOff-Strategy
-        int[] timeDelays = { 0, 25, 50, 75, 100 };
+        int[] timeDelays = { 0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75 };
         float[] percentagesOfPopulation = { 0.5f };
 
         for (int timeDelay : timeDelays) {
