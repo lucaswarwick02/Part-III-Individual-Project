f6d70350a34bb5a46e4f98e2f2d62bc3cce69bea
Lucas <lucaswarwick02@gmail.com>
2023-02-16 20:49:43 +0000

    Add oneoff 25 and 75

--- network-models/src/main/java/com/lucaswarwick02/mains/GridSearchMain.java
+++ network-models/src/main/java/com/lucaswarwick02/mains/GridSearchMain.java
@@ -40,7 +40,7 @@ public class GridSearchMain {
 
         // * OneOff-Strategy
         int[] timeDelays = { 0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75 };
-        float[] percentagesOfPopulation = { 0.5f };
+        float[] percentagesOfPopulation = { 0.75f };
 
         for (int timeDelay : timeDelays) {
             for (float percentageOfPopulation : percentagesOfPopulation) {
