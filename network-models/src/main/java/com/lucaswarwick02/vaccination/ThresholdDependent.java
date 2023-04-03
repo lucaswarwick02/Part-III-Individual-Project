625c775ef968633993b07ee5bf4b0a00eea560a1
Lucas <lucaswarwick02@gmail.com>
2023-04-03 16:32:13 +0100

    Add fucntionality to ThresholdDependent strategy

--- network-models/src/main/java/com/lucaswarwick02/vaccination/ThresholdDependent.java
+++ network-models/src/main/java/com/lucaswarwick02/vaccination/ThresholdDependent.java
@@ -1,5 +1,9 @@
 package com.lucaswarwick02.vaccination;
 
+import java.util.List;
+
+import com.lucaswarwick02.components.ModelParameters;
+import com.lucaswarwick02.components.Node;
 import com.lucaswarwick02.models.StochasticModel;
 import com.lucaswarwick02.vaccination.StrategyFactory.StrategyType;
 
@@ -14,10 +18,17 @@ public class ThresholdDependent extends AbstractStrategy {
 
     @Override
     public void performStrategy(StochasticModel model) {
-        // TODO Check the total number of infected individuals
-        // TODO Convert to percentage of population
-        // TODO If the percentage is greater than or equal to the threshold, vaccinate
-        // TODO ... using oldest first
+        if (model.getCurrentTime() == 0)
+            return;
+
+        float infectedPercentage = model.totals.get("Infected")[model.getCurrentTime() - 1]
+                / (float) ModelParameters.NUMBER_OF_NODES;
+
+        if (infectedPercentage >= this.threshold) {
+            int numberOfNodes = (int) Math.floor(model.getUnderlyingNetwork().getNodes().size() * this.rho);
+            List<Node> nodesToVaccinate = model.getUnderlyingNetwork().getNodeByAge(numberOfNodes, true);
+            nodesToVaccinate.forEach(node -> node.setState(Node.State.VACCINATED));
+        }
     }
 
     @Override
